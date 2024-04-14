3ce2273c5f24968339d378e75df568363c887a7d
Lefteris <107320265+Zaga-n@users.noreply.github.com>
2024-04-14 21:15:44 +0300

    Add files via upload

---
+++ chunk.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include "chunk.h"
+#include "memory.h"
+#include "vm.h"
+
+void initChunk(Chunk *chunk)
+{
+    chunk->count = 0;
+    chunk->capacity = 0;
+    chunk->code = NULL;
+    chunk->lines = NULL;
+    initValueArray(&chunk->constants);
+}
+void freeChunk(Chunk *chunk)
+{
+    FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
+    freeValueArray(&chunk->constants);
+    FREE_ARRAY(int, chunk->lines, chunk->capacity);
+    initChunk(chunk);
+}
+void writeChunk(Chunk *chunk, uint8_t byte, int line)
+{
+    if (chunk->capacity < chunk->count + 1)
+    {
+        int oldCapacity = chunk->capacity;
+        chunk->capacity = GROW_CAPACITY(oldCapacity);
+        chunk->lines = GROW_ARRAY(int, chunk->lines, oldCapacity, chunk->capacity);
+        chunk->code = GROW_ARRAY(uint8_t, chunk->code, oldCapacity, chunk->capacity);
+    }
+    chunk->code[chunk->count] = byte;
+    chunk->count++;
+    chunk->lines[chunk->count] = line;
+}
+int addConstant(Chunk *chunk, Value value)
+{
+    push(value);
+    writeValueArray(&chunk->constants, value);
+    pop();
+    return chunk->constants.count - 1;
+}
